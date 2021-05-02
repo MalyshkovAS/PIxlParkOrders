9c6a107fb5c208a48e01eb238e0a8d68bcbd95ad
Алексей Малышков <Mywellex@gmail.com>
2021-05-02 16:58:31 +0700

    Добавьте файлы проекта.

---
+++ WebApplication2/Order.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WebApplication2
+{
+    public class DeliveryAddress
+    {
+        public string ZipCode { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class Shipping
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class Result
+    {
+
+        public string Id { get; set; }
+        public int PhotolabId { get; set; }
+        public string CustomId { get; set; }
+        public object SourceOrderId { get; set; }
+        public string ManagerId { get; set; }
+        public object AssignedToId { get; set; }
+        public string Title { get; set; }
+        public string TrackingUrl { get; set; }
+        public object TrackingNumber { get; set; }
+        public string Status { get; set; }
+        public string RenderStatus { get; set; }
+        public string PaymentStatus { get; set; }
+        public DeliveryAddress DeliveryAddress { get; set; }
+        public Shipping Shipping { get; set; }
+        public int CommentsCount { get; set; }
+        public string DownloadLink { get; set; }
+        public string PreviewImageUrl { get; set; }
+        public double Price { get; set; }
+        public double DiscountPrice { get; set; }
+        public double DeliveryPrice { get; set; }
+        public double TotalPrice { get; set; }
+        public double PaidPrice { get; set; }
+        public int UserId { get; set; }
+        public int? UserCompanyAccountId { get; set; }
+        public string DiscountTitle { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public DateTime? DatePaid { get; set; }
+        public DateTime? DateReady { get; set; }
+        public string LastDownloadedPaymentDocument { get; set; }
+        public string PaymentSystemUniqueId { get; set; }
+        public string GoogleClientId { get; set; }
+        public object ContractorOrderNumber { get; set; }
+        public double ContractorOrderTotalPrice { get; set; }
+    }
+
+    public class Root
+    {
+        public string ApiVersion { get; set; }
+        public List<Result> Result { get; set; }
+        public int ResponseCode { get; set; }
+    }
+
+}
---
+++ WebApplication2/PixlParkApi.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace WebApplication2
+{
+    public class PixlParkApi
+    {
+        private string publicKey
+        {
+            get
+            {
+                return "38cd79b5f2b2486d86f562e3c43034f8";
+            }
+        }
+        private string privateKey
+        {
+            get
+            {
+                return "8e49ff607b1f46e1a5e8f6ad5d312a80";
+            }
+        }
+        private string reqToken { get; set; }
+        private string password { get; set; }
+        private string accessToken { get; set; }
+
+        string Hash(string input)
+        {
+            var hash = new SHA1Managed().ComputeHash(System.Text.Encoding.UTF8.GetBytes(input));
+            return string.Concat(hash.Select(b => b.ToString("x2")));
+        }
+        private void GetReqToken()
+        {
+            WebClient webClient = new WebClient();
+            string URL = "http://api.pixlpark.com/oauth/requesttoken";
+            string response = webClient.DownloadString(URL);
+            JObject jsonObj = JObject.Parse(response);
+            reqToken = jsonObj.GetValue("RequestToken").ToString();
+
+        }
+        public void GetAccessToken()
+        {
+            GetReqToken();
+            password = Hash(reqToken + privateKey);
+            WebClient webClient = new WebClient();
+            webClient.QueryString.Add("oauth_token", reqToken);
+            webClient.QueryString.Add("grant_type", "api");
+            webClient.QueryString.Add("username", publicKey);
+            webClient.QueryString.Add("password", password);
+            string URL = "http://api.pixlpark.com/oauth/accesstoken";
+            string response = webClient.DownloadString(URL);
+            JObject jsonObj = JObject.Parse(response);
+            accessToken = jsonObj.GetValue("AccessToken").ToString();
+        }
+        public Root GetOrderList()
+        {
+
+            WebClient webClient = new WebClient();
+            webClient.QueryString.Add("oauth_token", accessToken);
+            string URL = "http://api.pixlpark.com/orders";
+            string response = webClient.DownloadString(URL);
+
+            Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(response);
+            return myDeserializedClass;
+        }
+    }
+}
